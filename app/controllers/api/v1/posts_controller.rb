b17d9f23017a79662c131093cf4cf4e7d5ba6cc9
Edgar Ferreira <efsn88@protonmail.com>
2024-11-19 13:42:02 -0300

    Correct posts_url from PostsController and test.

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,30 +1,29 @@
 class Api::V1::PostsController < ApplicationController
-  before_action :set_post, only: %i[ show update destroy ]
+  before_action :set_post, only: %i[show update destroy]
 
-  # GET /posts
+  # GET /api/v1/posts
   def index
     @posts = Post.order(created_at: :desc)
-
     render json: @posts
   end
 
-  # GET /posts/1
+  # GET /api/v1/posts/1
   def show
     render json: @post
   end
 
-  # POST /posts
+  # POST /api/v1/posts
   def create
     @post = Post.new(post_params)
 
     if @post.save
-      render json: @post, status: :created, location: @post
+      render json: @post, status: :created, location: api_v1_post_url(@post)
     else
       render json: @post.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /posts/1
+  # PATCH/PUT /api/v1/posts/1
   def update
     if @post.update(post_params)
       render json: @post
@@ -33,19 +32,20 @@ class Api::V1::PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
+  # DELETE /api/v1/posts/1
   def destroy
     @post.destroy!
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.expect(post: [ :title, :body ])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
